61f212c8ad1b32e47db5a19db28db634a05978fa
Feres Ben Sassi <feres.bensassi@avaxia-group.com>
2023-03-13 15:50:18 +0100

    redme.md file update

---
+++ src/main/java/tn/esprit/feresski/repositories/ProjectRepository.java
@@ -0,0 +1,6 @@
+package tn.esprit.feresski.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository {
+}
---
+++ src/main/java/tn/esprit/feresski/repositories/SkieurRepository.java
@@ -0,0 +1,9 @@
+package tn.esprit.feresski.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tn.esprit.feresski.entities.Skieur;
+
+import javax.swing.text.html.parser.Entity;
+
+public interface SkieurRepository extends JpaRepository<Skieur, Integer> {
+}
